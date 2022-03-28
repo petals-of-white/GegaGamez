ab64372a67ed6f68e6facd5d9bf05609ebc4273c
Maksym Syvash <maxim.sivash@gmail.com>
2022-03-28 10:54:23 +0300

    working on async

--- GegaGamez.DAL/Repositories/EFCore/UserRepository.cs
+++ GegaGamez.DAL/Repositories/EFCore/UserRepository.cs
@@ -24,9 +24,33 @@ namespace GegaGamez.DAL.Repositories.EFCore
             return usersByUsername;
         }
 
+        public async Task<IEnumerable<User>> GetAllByUsernameAsync(string username)
+        {
+            // this anonymous comparer method might be replaced by something better in the future
+            var usernameSearcher = delegate (string inputUsername, string compareToUsername)
+            {
+                return compareToUsername.ToLower().Contains(inputUsername.ToLower());
+            };
+
+            var usersByUsername = await (from user in _dbSet
+                                         where usernameSearcher(username, user.Username)
+                                         select user).ToListAsync();
+
+            var task = await Task.FromResult(usersByUsername.AsEnumerable());
+
+            return task;
+        }
+
         public User? GetByCredentials(string username, string password)
         {
             User? user = _dbSet.SingleOrDefault(u => u.Username == username);
+
+            return user ?? throw new ArgumentException("User found, but the password was incorrect.", nameof(password));
+        }
+
+        public async Task<User?> GetByCredentialsAsync(string username, string password)
+        {
+            User? user = await _dbSet.SingleOrDefaultAsync(u => u.Username == username);
 
             return user ?? throw new ArgumentException("User found, but the password was incorrect.", nameof(password));
         }
