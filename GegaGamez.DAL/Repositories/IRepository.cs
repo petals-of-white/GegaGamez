dfb238c5f8eb096b89eb56c5a065356618c3dd06
Maksym Syvash <maxim.sivash@gmail.com>
2022-03-28 10:45:28 +0300

    irepository now contains async methods

--- GegaGamez.DAL/Repositories/IRepository.cs
+++ GegaGamez.DAL/Repositories/IRepository.cs
@@ -9,23 +9,39 @@ namespace GegaGamez.DAL.Repositories
     {
         void Add(TEntity entity);
 
+        Task AddAsync(TEntity entity);
+
         void AddRange(IEnumerable<TEntity> entities);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
 
         int Count();
 
         int CountAll(Expression<Func<TEntity, bool>> predicate);
 
+        Task<int> CountAllAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<int> CountAsync();
+
         TEntity? Get(int id);
 
         IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
 
+        Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity?> GetAsync(int id);
+
         IEnumerable<TEntity> List();
 
-        void Remove(TEntity entity);
+        Task<IEnumerable<TEntity>> ListAsync();
 
         void Remove(int id);
 
+        void Remove(TEntity entity);
+
         void RemoveAll(Expression<Func<TEntity, bool>> predicate);
+
+        Task RemoveAsync(int id);
 
         void RemoveRange(IEnumerable<TEntity> entities);
     }
