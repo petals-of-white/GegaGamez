85dceb0fb4f216eec0174b8fb5f0ad797306e6fa
Maksym Syvash <maxim.sivash@gmail.com>
2022-04-05 12:57:36 +0300

    added <ultipleValidationsException

---
+++ GegaGamez.BLL/Exceptions/MultipleValidationsException.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GegaGamez.BLL.Exceptions
+{
+    public class MultipleValidationsException : Exception
+    {
+        public MultipleValidationsException(string message, ICollection<ValidationResult> validationResults) : base(message)
+        {
+            ValidationResults = validationResults;
+        }
+
+        public ICollection<ValidationResult> ValidationResults { get; set; }
+    }
+}
