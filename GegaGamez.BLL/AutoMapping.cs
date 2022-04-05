76481e97ff33cb6384d581039a46061719c54b5f
Maksym Syvash <maxim.sivash@gmail.com>
2022-04-05 21:32:02 +0300

    Configured automapping

--- GegaGamez.BLL/AutoMapping.cs
+++ GegaGamez.BLL/AutoMapping.cs
@@ -8,10 +8,18 @@ namespace GegaGamez.BLL
 
         static AutoMapping()
         {
-            throw new NotImplementedException();
             var mapConfig = new MapperConfiguration(config =>
             {
-                //config.CreateMap<>();
+                config.CreateMap<DAL.Entities.Comment, Models.Comment>().ReverseMap();
+                config.CreateMap<DAL.Entities.Country, Models.Country>().ReverseMap();
+                config.CreateMap<DAL.Entities.DefaultCollection, Models.DefaultCollection>().ReverseMap();
+                config.CreateMap<DAL.Entities.DefaultCollectionType, Models.DefaultCollectionType>().ReverseMap();
+                config.CreateMap<DAL.Entities.Developer, Models.Developer>().ReverseMap();
+                config.CreateMap<DAL.Entities.Game, Models.Game>().ReverseMap();
+                config.CreateMap<DAL.Entities.Genre, Models.Genre>().ReverseMap();
+                config.CreateMap<DAL.Entities.Rating, Models.Rating>().ReverseMap();
+                config.CreateMap<DAL.Entities.User, Models.User>().ReverseMap();
+                config.CreateMap<DAL.Entities.UserCollection, Models.UserCollection>().ReverseMap();
             });
 
             Mapper = new Mapper(mapConfig);
