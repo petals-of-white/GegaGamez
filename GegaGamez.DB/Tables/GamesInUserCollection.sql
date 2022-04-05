96dd5b830c23532417a7525ceeca10a04830626d
Maksym Syvash <maxim.sivash@gmail.com>
2022-04-05 16:53:38 +0300

    Update GamesInUserCollection.sql

--- GegaGamez.DB/Tables/GamesInUserCollection.sql
+++ GegaGamez.DB/Tables/GamesInUserCollection.sql
@@ -2,7 +2,7 @@
 (
 	[UserCollectionId] INT NOT NULL , 
     [GameId] INT NOT NULL, 
-    PRIMARY KEY ([GameId], [UserCollectionId]), 
+    PRIMARY KEY ([UserCollectionId], [GameId]), 
     CONSTRAINT [FK_GamesInUserCollections_Collection] FOREIGN KEY ([UserCollectionId]) REFERENCES [UserCollection]([Id]), 
     CONSTRAINT [FK_GamesInUserCollections_Game] FOREIGN KEY ([GameId]) REFERENCES [Game]([Id])
 )
