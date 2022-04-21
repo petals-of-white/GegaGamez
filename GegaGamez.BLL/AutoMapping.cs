65951394597927e4b917f1d56671ac28cdc5d89e
Maksym Syvash <maxim.sivash@gmail.com>
2022-04-21 20:32:56 +0300

    Mapping types changed according to the new assembly

--- GegaGamez.BLL/AutoMapping.cs
+++ GegaGamez.BLL/AutoMapping.cs
@@ -10,16 +10,16 @@ namespace GegaGamez.BLL
         {
             var mapConfig = new MapperConfiguration(config =>
             {
-                config.CreateMap<DAL.Entities.Comment, Models.Comment>().ReverseMap();
-                config.CreateMap<DAL.Entities.Country, Models.Country>().ReverseMap();
-                config.CreateMap<DAL.Entities.DefaultCollection, Models.DefaultCollection>().ReverseMap();
-                config.CreateMap<DAL.Entities.DefaultCollectionType, Models.DefaultCollectionType>().ReverseMap();
-                config.CreateMap<DAL.Entities.Developer, Models.Developer>().ReverseMap();
-                config.CreateMap<DAL.Entities.Game, Models.Game>().ReverseMap();
-                config.CreateMap<DAL.Entities.Genre, Models.Genre>().ReverseMap();
-                config.CreateMap<DAL.Entities.Rating, Models.Rating>().ReverseMap();
-                config.CreateMap<DAL.Entities.User, Models.User>().ReverseMap();
-                config.CreateMap<DAL.Entities.UserCollection, Models.UserCollection>().ReverseMap();
+                config.CreateMap<DAL.Entities.Comment, Shared.BusinessModels.Comment>().ReverseMap();
+                config.CreateMap<DAL.Entities.Country, Shared.BusinessModels.Country>().ReverseMap();
+                config.CreateMap<DAL.Entities.DefaultCollection, Shared.BusinessModels.DefaultCollection>().ReverseMap();
+                config.CreateMap<DAL.Entities.DefaultCollectionType, Shared.BusinessModels.DefaultCollectionType>().ReverseMap();
+                config.CreateMap<DAL.Entities.Developer, Shared.BusinessModels.Developer>().ReverseMap();
+                config.CreateMap<DAL.Entities.Game, Shared.BusinessModels.Game>().ReverseMap();
+                config.CreateMap<DAL.Entities.Genre, Shared.BusinessModels.Genre>().ReverseMap();
+                config.CreateMap<DAL.Entities.Rating, Shared.BusinessModels.Rating>().ReverseMap();
+                config.CreateMap<DAL.Entities.User, Shared.BusinessModels.User>().ReverseMap();
+                config.CreateMap<DAL.Entities.UserCollection, Shared.BusinessModels.UserCollection>().ReverseMap();
             });
 
             Mapper = new Mapper(mapConfig);
