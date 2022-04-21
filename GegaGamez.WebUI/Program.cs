734529ac79d409d786535a593064c3de3f31bae9
Maksym Syvash <maxim.sivash@gmail.com>
2022-04-21 20:35:37 +0300

    Updated Program.cs to with dependency injection

--- GegaGamez.WebUI/Program.cs
+++ GegaGamez.WebUI/Program.cs
@@ -1,7 +1,25 @@
+using GegaGamez.BLL.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// connectino string
+string connectionString = builder.Configuration.GetConnectionString("Default");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//builder.Services.AddScoped(service => new GameService(connectionString));
+//builder.Services.AddScoped(service => new CollectionsBL(connectionString));
+//builder.Services.AddScoped(service => new CommentsBL(connectionString));
+//builder.Services.AddScoped(service => new GenreBL(connectionString));
+//builder.Services.AddScoped(service => new RatingBL(connectionString));
+//builder.Services.AddScoped(service => new UserService(connectionString));
+//builder.Services.AddScoped(service => new DeveloperBL(connectionString));
+
+builder.Services.AddScoped(service => new UserService(connectionString));
+builder.Services.AddScoped(service => new GameService(connectionString));
+builder.Services.AddScoped(service => new GameCollectionService(connectionString));
+builder.Services.AddScoped(service => new GenreService(connectionString));
+builder.Services.AddScoped(service => new DeveloperService(connectionString));
 
 var app = builder.Build();
 
@@ -18,6 +36,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
