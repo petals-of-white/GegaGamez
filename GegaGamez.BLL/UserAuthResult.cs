e3c04e8fa5f067e2b44d1745c82f2492724d2b3b
Maksym Syvash <maxim.sivash@gmail.com>
2022-04-21 20:34:13 +0300

    Create UserAuthResult.cs
    Represents User auth result (along with User model)

---
+++ GegaGamez.BLL/UserAuthResult.cs
@@ -0,0 +1,17 @@
+﻿using GegaGamez.BLL.Enums;
+using GegaGamez.Shared.BusinessModels;
+
+namespace GegaGamez.BLL
+{
+    public class UserAuthResult
+    {
+        internal UserAuthResult(User? user, AuthStatus status)
+        {
+            Status = status;
+            User = user;
+        }
+
+        public AuthStatus Status { get; internal init; }
+        public User? User { get; internal init; }
+    }
+}
