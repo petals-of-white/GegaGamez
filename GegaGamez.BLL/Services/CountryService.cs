23cceec63e8f69a2de930530b4c8c58c5fc8f1ad
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-02 16:05:57 +0300

    Create CountryService.cs

---
+++ GegaGamez.BLL/Services/CountryService.cs
@@ -0,0 +1,17 @@
+﻿using GegaGamez.Shared.DataAccess;
+using GegaGamez.Shared.Entities;
+using GegaGamez.Shared.Services;
+
+namespace GegaGamez.BLL.Services;
+
+public class CountryService : ICountryService
+{
+    private readonly IUnitOfWork _db;
+
+    public CountryService(IUnitOfWork db)
+    {
+        _db = db;
+    }
+
+    public IEnumerable<Country> AllCountries() => _db.Countries.List();
+}
