df5d81c76d285e37c2d45ddf77977b463785b945
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-02 16:04:24 +0300

    Added validators

---
+++ GegaGamez.WebUI/Models/Validation/RegisterValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using GegaGamez.WebUI.Models.Auth;
+
+namespace GegaGamez.WebUI.Models.Validation;
+
+public class RegisterValidator : AbstractValidator<RegisterUserModel>
+{
+    public RegisterValidator()
+    {
+        RuleFor(r => r.Username).Length(6, 30);
+        RuleFor(r => r.Password).Length(6, 30);
+        RuleFor(r => r.ConfirmPassword).Length(6, 30);
+        RuleFor(r => r.PasswordsMatch).Must((r, _) => r.PasswordsMatch == true);
+    }
+}
---
+++ GegaGamez.WebUI/Models/Validation/UpdateProfileValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using GegaGamez.WebUI.Models.ModifyModels;
+
+namespace GegaGamez.WebUI.Models.Validation
+{
+    public class UpdateProfileValidator : AbstractValidator<UpdateProfileModel>
+    {
+        public UpdateProfileValidator()
+        {
+            RuleFor(profile => profile.Id).NotEmpty();
+            RuleFor(profile => profile.CountryId).NotEmpty();
+            RuleFor(profile => profile.About).MaximumLength(500).NotEmpty();
+        }
+    }
+}
