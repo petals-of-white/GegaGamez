5116a850d989c3e3000f4f83f7d7aa13256d0055
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-02 16:06:16 +0300

    Create ServiceCollectionExtensions.cs
    Contains Extensions methods for DI container

---
+++ GegaGamez.BLL/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using GegaGamez.BLL.Services;
+using GegaGamez.DAL;
+using GegaGamez.Shared.DataAccess;
+using GegaGamez.Shared.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GegaGamez.BLL
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds EFCore UnitOfWork (SQL Server)
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddGegaGamezDB(this IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>(serviceProv => new UnitOfWork(connectionString));
+            return services;
+        }
+
+        /// <summary>
+        /// Adds db-driven services
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddGegaGamezServices(this IServiceCollection services)
+        {
+            services
+                .AddTransient<IUserService, UserService>()
+                .AddTransient<IGameService, GameService>()
+                .AddTransient<IGameCollectionService, GameCollectionService>()
+                .AddTransient<IGenreService, GenreService>()
+                .AddTransient<IDeveloperService, DeveloperService>()
+                .AddTransient<ICountryService, CountryService>();
+
+            return services;
+        }
+    }
+}
