c5dfab8d18b274260c0aeaf0b870a31a03a9aeb8
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-02 16:04:14 +0300

    Added models for user forms

---
+++ GegaGamez.WebUI/Models/ModifyModels/NewCommentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GegaGamez.WebUI.Models.ModifyModels
+{
+    public record class NewCommentModel
+    {
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ GegaGamez.WebUI/Models/ModifyModels/UpdateProfileModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GegaGamez.WebUI.Models.ModifyModels;
+
+public record class UpdateProfileModel
+{
+    //public string Username { get; set; }
+    //public string? Name { get; set; }
+    public int Id { get; set; }
+    public int CountryId { get; set; }
+    public string About { get; set; }
+}
