88279a03185931cb19fbeb727cda10f6202834e9
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-02 16:07:44 +0300

    Added auth models

---
+++ GegaGamez.WebUI/Models/Auth/LoginUserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GegaGamez.WebUI.Models.Auth;
+
+public record class LoginUserModel
+{
+    public string Username { get; init; }
+    public string Password { get; init; }
+}
---
+++ GegaGamez.WebUI/Models/Auth/RegisterUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GegaGamez.WebUI.Models.Auth;
+
+public record class RegisterUserModel
+{
+    public string Username { get; init; }
+    public string Password { get; init; }
+    public string ConfirmPassword { get; init; }
+    public bool PasswordsMatch => Password == ConfirmPassword;
+}
