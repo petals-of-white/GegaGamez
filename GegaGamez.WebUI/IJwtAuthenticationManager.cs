6be861a555a761a9d7e9c64c8ec9980da1cbb121
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-04 02:02:56 +0300

    jwt auth manager update

--- GegaGamez.WebUI/IJwtAuthenticationManager.cs
+++ GegaGamez.WebUI/IJwtAuthenticationManager.cs
@@ -1,14 +1,9 @@
-﻿using GegaGamez.Shared;
-using GegaGamez.WebUI.Models.Display;
+﻿using GegaGamez.WebUI.Models.Display;
 
 namespace GegaGamez.WebUI
 {
     public interface IJwtAuthenticationManager
     {
         (string cookieName, string tokenValue, CookieOptions cookieOptions) SignInUser(UserModel user, bool RememberMe = true);
-
-        public UserAuthResult AuthenticateUser(string username, string password);
-
-        public UserAuthResult RegisterUser(string username, string password);
     }
 }
