e12319cbaa56bd92d3dacd7b5fec2c31f18b6ef6
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-12 20:20:58 +0300

    Updated db with new Role And UserRole tables

---
+++ GegaGamez.DB/Tables/UserRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[UserId] INT NOT NULL , 
+    [RoleId] INT NOT NULL, 
+    CONSTRAINT [PK_UserRole] PRIMARY KEY ([UserId], [RoleId]), 
+    CONSTRAINT [FK_UserRole_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]), 
+    CONSTRAINT [FK_UserRole_Role] FOREIGN KEY ([RoleId]) REFERENCES [Role]([Id]), 
+)
