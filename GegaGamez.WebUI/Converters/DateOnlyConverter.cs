e795cccac3dd8c408e4f8dad885f8329c3a30858
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-12 14:26:45 +0300

    dateonly converters

---
+++ GegaGamez.WebUI/Converters/DateOnlyConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GegaGamez.WebUI.Converters;
+
+public class DateOnlyConverter : JsonConverter<DateOnly>
+{
+    private readonly string serializationFormat;
+    public DateOnlyConverter() : this(null)
+    {
+
+    }
+
+    public DateOnlyConverter(string? serializationFormat)
+    {
+        serializationFormat = serializationFormat ?? "dd.MM.yyyy";
+    }
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return DateOnly.Parse(value!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+}
