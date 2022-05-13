e6cae3f3a20ea7d8c71d02cb79df851fe83fab32
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-13 17:35:26 +0300

    added roles and policies static classes

---
+++ GegaGamez.WebUI/Security/PolicyNames.cs
@@ -0,0 +1,8 @@
+﻿namespace GegaGamez.WebUI.Security;
+
+internal static class PolicyNames
+{
+    public const string AdminPolicy = "AdminPolicy";
+    public const string UserPolicy = "UserPolicy";
+    public const string GuestPolicy = "GuestPolicy";
+}
