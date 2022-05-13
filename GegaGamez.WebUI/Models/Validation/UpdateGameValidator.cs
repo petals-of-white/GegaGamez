5f2c6a02f67cce70bff4ed59282ec90ef9df7111
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-13 17:36:48 +0300

    Create UpdateGameValidator.cs

---
+++ GegaGamez.WebUI/Models/Validation/UpdateGameValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using GegaGamez.WebUI.Models.ModifyModels;
+
+namespace GegaGamez.WebUI.Models.Validation;
+
+public class UpdateGameValidator : AbstractValidator<UpdateGameModel>
+{
+    public UpdateGameValidator()
+    {
+        RuleFor(g => g.Title).NotEmpty().MaximumLength(100);
+        RuleFor(g => g.Description).NotEmpty().MaximumLength(1000);
+        RuleFor(g => g.ReleaseDate).NotNull();
+        RuleFor(g => g.DeveloperId).NotEmpty();
+    }
+}
