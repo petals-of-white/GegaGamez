419b9a7b49417d63edb505bbebe044435f7df54d
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-13 17:35:54 +0300

    New updatemodels for future admins

---
+++ GegaGamez.WebUI/Models/ModifyModels/UpdateDeveloperModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GegaGamez.WebUI.Models.ModifyModels;
+
+public record class UpdateDeveloperModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateOnly BeginDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
---
+++ GegaGamez.WebUI/Models/ModifyModels/UpdateGameModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GegaGamez.WebUI.Models.ModifyModels;
+
+public record class UpdateGameModel
+{
+    public string Title { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public string Description { get; set; }
+    public int DeveloperId { get; set; }
+}
