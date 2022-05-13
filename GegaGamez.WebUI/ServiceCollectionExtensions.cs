62f930f542d9220b0ee1a817c75bdcef248273ad
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-13 17:37:19 +0300

    Update ServiceCollectionExtensions.cs

--- GegaGamez.WebUI/ServiceCollectionExtensions.cs
+++ GegaGamez.WebUI/ServiceCollectionExtensions.cs
@@ -1,49 +1,65 @@
-﻿using System.Text;
-using System.Text.Json;
-using GegaGamez.WebUI.Auth;
+﻿using System.Text.Json;
 using GegaGamez.WebUI.Converters;
+using GegaGamez.WebUI.Security;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 namespace GegaGamez.WebUI
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddGegaGamezSecurity(this IServiceCollection services, string securityKey)
+        public static IServiceCollection AddGegaGamezSecurity(this IServiceCollection services)
         {
-            services.AddScoped<IJwtAuthenticationManager>(service =>
-            {
-                return new JwtAuthenticationManager(securityKey);
-            });
+            //services.AddScoped<IJwtAuthenticationManager>(service =>
+            //{
+            //    return new JwtAuthenticationManager(securityKey);
+            //});
 
             services.AddScoped<IAuthManager, AuthManager>();
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                    .AddCookie()
-
-                    .AddJwtBearer((o =>
+                    .AddCookie(options =>
                     {
-                        o.RequireHttpsMetadata = false;
-                        o.SaveToken = true;
+                        options.LoginPath = "/Login";
+                        options.AccessDeniedPath = "/AccessDenied";
+                    });
 
-                        o.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ValidateIssuerSigningKey = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey)),
-                            ValidateIssuer = false,
-                            ValidateAudience = false,
-                        };
+            /*
+            .AddJwtBearer((o =>
+            {
+                o.RequireHttpsMetadata = false;
+                o.SaveToken = true;
 
-                        o.Events = new JwtBearerEvents
-                        {
-                            OnMessageReceived = context =>
-                            {
-                                context.Token = context.Request.Cookies [JwtAuthenticationManager.CookieName];
-                                return Task.CompletedTask;
-                            }
-                        };
-                    }));
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+
+                o.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies [JwtAuthenticationManager.CookieName];
+                        return Task.CompletedTask;
+                    }
+                };
+            }));
+            */
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(PolicyNames.UserPolicy, config =>
+                {
+                    config.RequireRole(Roles.User);
+                });
+
+                options.AddPolicy(PolicyNames.AdminPolicy, config =>
+                {
+                    config.RequireRole(Roles.Admin);
+                });
+            });
 
             return services;
         }
