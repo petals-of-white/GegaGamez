53e8bc426c755f545c39a2e02571135ede83cf67
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-15 19:01:46 +0300

    Create RoleRepository.cs

---
+++ GegaGamez.DAL/Repositories/RoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using GegaGamez.Shared.DataAccess.Repositories;
+using GegaGamez.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GegaGamez.DAL.Repositories;
+public class RoleRepository : Repository<Role>, IRoleRepository
+{
+
+    public RoleRepository(DbContext dbContext):base(dbContext)
+    {
+
+    }
+}
