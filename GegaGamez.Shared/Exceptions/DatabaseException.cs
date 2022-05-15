7801ef563c80dfaf68a3bed163af0a0ff32a2aa4
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-15 19:03:20 +0300

    Create DatabaseException.cs

---
+++ GegaGamez.Shared/Exceptions/DatabaseException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace GegaGamez.Shared.Exceptions;
+
+public class DatabaseException : Exception
+{
+    protected DatabaseException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public DatabaseException()
+    {
+    }
+
+    public DatabaseException(string? message) : base(message)
+    {
+    }
+
+    public DatabaseException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
