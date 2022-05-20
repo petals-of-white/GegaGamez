840dc7998c0b52f7464adca36c013212e2e3f4af
Maksym Syvash <maxim.sivash@gmail.com>
2022-05-21 01:33:30 +0300

    Added CreatedAt property

--- GegaGamez.WebUI/Models/Display/CommentModel.cs
+++ GegaGamez.WebUI/Models/Display/CommentModel.cs
@@ -3,10 +3,8 @@
 public record class CommentModel
 {
     public int Id { get; set; }
-
-    //public int GameId { get; set; }
-    //public int UserId { get; set; }
     public UserModel User { get; set; }
     public GameModel Game { get; set; }
     public string Text { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
